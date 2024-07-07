34eb4176576d6412fa3636bbbc51cb81c770b5bb
wolfiee42 <saifalislam2022@gmail.com>
2024-07-07 21:47:09 +0600

    fixed some bug

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -5,7 +5,7 @@ import { authValidation } from "./auth.validation";
 
 const route = Router();
 
-route.post('/sign-up', validateRequest(authValidation.createAdminUserValidation), authController.createAdminUser)
+route.post('/signup', validateRequest(authValidation.createAdminUserValidation), authController.createAdminUser)
 route.post('/login', validateRequest(authValidation.loginValidattion), authController.login)
 
 export const authRoutes = route;
